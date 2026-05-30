109316982401a9d008a37647562cff2751b779cb
nadiaaa066 <nadia.sreekumar@student.uts.edu.au>
2026-05-31 02:48:01 +1000

    Update entrypoint.sh

--- .github/scripts/entrypoint.sh
+++ .github/scripts/entrypoint.sh
@@ -1,1 +1,17 @@
+#!/bin/bash
 
+echo "Running todo.py..."
+
+python3 /app/.github/scripts/todo.py | tee /tmp/tasks.txt
+
+echo "Running unit tests..."
+
+python3 /app/.github/scripts/todo-test.py | tee /tmp/tests.txt
+
+echo "Updating index.html..."
+
+bash /app/.github/scripts/update_index.sh \
+    /tmp/tasks.txt \
+    /tmp/tests.txt
+
+echo "Process completed."
